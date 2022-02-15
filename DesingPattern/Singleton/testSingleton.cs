d5b73a4b583d23449be4550f8a699500ec9db2ac
ale <alejandroferllub@gmail.com>
2022-02-14 21:37:56 -0400

    type: first commint

---
+++ DesingPattern/Singleton/testSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Singleton
+{
+    internal class testSingleton
+    {
+        //this is the instance
+        private static testSingleton? _instance;
+        private static readonly object _door = new object();
+        public String message = "";
+
+        protected testSingleton() 
+        {
+            this.message = "hello world";
+        }
+
+        //method
+        public static testSingleton instance 
+        {
+            get 
+            {
+                if (_instance == null) 
+                {
+                    lock (_door) 
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new testSingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ DesingPattern/ejecutor.cs
@@ -0,0 +1,21 @@
+﻿using DesingPattern.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern
+{
+    internal class ejecutor
+    {
+        static void Main(string[] args) 
+        {
+            var v = testSingleton.instance;
+            Console.WriteLine(v.message);
+            v.message = "hi";
+            var f = testSingleton.instance;
+            Console.WriteLine(f.message);
+        }
+    }
+}
