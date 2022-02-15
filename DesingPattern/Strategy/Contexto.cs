17ad5c79f1fa155b7f692dfb5d2502aa68a07be3
ale <alejandroferllub@gmail.com>
2022-02-15 02:07:29 -0400

    type: add Strategy desing pattern

---
+++ DesingPattern/Strategy/Contexto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Strategy
+{
+    internal class Contexto
+    {
+        private love men;
+
+        public Contexto() 
+        {
+            //Strategy default
+            this.men = new StrategyEye();
+        }
+
+        public void eyesContext() 
+        {
+            this.men = new StrategyEye();
+        }
+
+        public void dinnerContext() 
+        {
+            this.men = new StrategyDinner();
+        }
+
+        public void conquer() 
+        {
+            this.men.conquer();
+        }
+    }
+}
--- DesingPattern/ejecutor.cs
+++ DesingPattern/ejecutor.cs
@@ -1,5 +1,6 @@
 ﻿using DesingPattern.Prototype;
 using DesingPattern.Singleton;
+using DesingPattern.Strategy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,33 +9,49 @@ using System.Threading.Tasks;
 
 namespace DesingPattern
 {
-    internal class ejecutor 
+    internal class ejecutor
     {
         
-        static void Main(string[] args) 
+
+        static void Main(string[] args)
         {
             //sin();
             //proto();
+            stra();
+        }
+
+        //only for Strategy
+        static void stra() 
+        {
+            Contexto m1 = new Contexto();
+            m1.conquer();
+            m1.dinnerContext();
+            m1.eyesContext(); 
         }
 
         //only for prototype
-        static void proto() 
+        static void proto()
         {
-            Cars honda = new Cars() { doors = 2, motor = "v8"};
+            Cars honda = new Cars() { doors = 2, motor = "v8" };
             Cars honda_fake = (Cars)honda.Clone();
             honda_fake.motor = "v5";
-            Console.WriteLine("this car has "+honda.doors+" door/s and has a "+honda.motor+" motor");
-            Console.WriteLine("this car has "+honda_fake.doors+" door/s and has a "+honda_fake.motor+" motor");
-            Console.WriteLine("are theres cars the same?:{0}",honda == honda_fake);
+            Console.WriteLine("this car has " + honda.doors + " door/s and has a " + honda.motor + " motor");
+            Console.WriteLine("this car has " + honda_fake.doors + " door/s and has a " + honda_fake.motor + " motor");
+            Console.WriteLine("are theres cars the same?:{0}", honda == honda_fake);
         }
 
         //only for singleton
-        static void sin() {
+        static void sin()
+        {
             var v = testSingleton.instance;
             Console.WriteLine(v.message);
             v.message = "hi";
             var f = testSingleton.instance;
             Console.WriteLine(f.message);
         }
+
     }
+
 }
+
+
