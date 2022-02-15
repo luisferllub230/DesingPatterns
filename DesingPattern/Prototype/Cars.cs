d835e050db84eef19f6b17848a9a29e43d8d4f64
ale <alejandroferllub@gmail.com>
2022-02-14 23:28:36 -0400

    type: add prototype and modify ejecutor

---
+++ DesingPattern/Prototype/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Prototype
+{
+    internal class Cars : ICloneable
+    {
+        //.net has a interface for prototype desing pattern, it name is ICloneable  
+        public int doors { get; set;}
+        public String? motor { get; set;}
+
+        public object Clone()
+        {
+            return (Cars)this.MemberwiseClone();
+        }
+    }
+}
