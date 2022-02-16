176a30fadc726db79f87030c7c8ca455b472c0b0
ale <alejandroferllub@gmail.com>
2022-02-15 20:19:04 -0400

    type: arreglado

--- DesingPattern/ejecutor.cs
+++ DesingPattern/ejecutor.cs
@@ -1,9 +1,6 @@
 ﻿using DesingPattern.Prototype;
 using DesingPattern.Singleton;
-<<<<<<< HEAD
 using DesingPattern.Strategy;
-=======
->>>>>>> main
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +11,6 @@ namespace DesingPattern
 {
     internal class ejecutor 
     {
-        
-<<<<<<< HEAD
 
         static void Main(string[] args)
         {
@@ -25,7 +20,7 @@ namespace DesingPattern
         }
 
         //only for Strategy
-        static void stra() 
+        static void stra()
         {
             Contexto m1 = new Contexto();
             m1.conquer();
@@ -42,27 +37,6 @@ namespace DesingPattern
             Console.WriteLine("this car has " + honda.doors + " door/s and has a " + honda.motor + " motor");
             Console.WriteLine("this car has " + honda_fake.doors + " door/s and has a " + honda_fake.motor + " motor");
             Console.WriteLine("are theres cars the same?:{0}", honda == honda_fake);
-        }
-
-        //only for singleton
-        static void sin()
-=======
-        static void Main(string[] args) 
->>>>>>> main
-        {
-            //sin();
-            //proto();
-        }
-
-        //only for prototype
-        static void proto() 
-        {
-            Cars honda = new Cars() { doors = 2, motor = "v8"};
-            Cars honda_fake = (Cars)honda.Clone();
-            honda_fake.motor = "v5";
-            Console.WriteLine("this car has "+honda.doors+" door/s and has a "+honda.motor+" motor");
-            Console.WriteLine("this car has "+honda_fake.doors+" door/s and has a "+honda_fake.motor+" motor");
-            Console.WriteLine("are theres cars the same?:{0}",honda == honda_fake);
         }
 
         //only for singleton
