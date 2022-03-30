e0b860e517bd983bd0441816a52799b45053b706
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-03-30 14:22:38 -0400

    type: factory method

---
+++ DesingPattern/Factory/FactoryConnection.cs
@@ -0,0 +1,32 @@
+﻿using DesingPattern.Factory.dataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Factory
+{
+    internal class FactoryConnection
+    {
+        public IConnection Connection(String DB){
+
+            if (DB == null) {
+                return new ConnectionClean();
+            }
+            else if (DB == "SQLSERVER") {
+                return new SqlServer();
+            }
+            else if (DB == "MYSQL")
+            {
+                return new MySql();
+            }
+            else if (DB == "MARIADB")
+            {
+                return new mariadb();
+            }
+
+            return new ConnectionClean();
+        }
+    }
+}
---
+++ DesingPattern/Factory/dataBase/MySql.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPattern.Factory
+{
+    public class MySql : IConnection
+    {
+        private String host;
+        private String port;
+        private String username;
+        private String password;
+
+
+        public MySql()
+        {
+            this.host = "localhost";
+            this.port = "5433";
+            this.username = "admin";
+            this.password = "admin";
+        }
+
+     
+        void IConnection.connect()
+        {
+            Console.WriteLine("the user has connect with MySql");
+        }
+
+        void IConnection.disconnect()
+        {
+            Console.WriteLine("the user has disconnect with MySql");
+        }
+    }
+}
